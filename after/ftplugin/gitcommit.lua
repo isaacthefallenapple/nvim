9bd3db68d031620fd2c7409a22a34dfce3d58372
isaacthefallenapple <isaacthefallenapple@gmail.com>
2023-11-21 14:02:30 +0100

    use `vim.opt_local` instead of `vim.opt|vim.o`

--- after/ftplugin/gitcommit.lua
+++ after/ftplugin/gitcommit.lua
@@ -1,7 +1,7 @@
-vim.opt.scrolloff = 0
-vim.opt.textwidth = 80
+vim.opt_local.scrolloff = 0
+vim.opt_local.textwidth = 80
 
-vim.opt.formatexpr = ''
-vim.opt.formatprg = ''
+vim.opt_local.formatexpr = ''
+vim.opt_local.formatprg = ''
 
-vim.opt.formatoptions:append { 't', 'a' }
+vim.opt_local.formatoptions:append { 't', 'a' }
