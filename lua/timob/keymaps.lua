bc852ed2ed3d48d59c468353096b6e7a41a3df75
isaacthefallenapple <isaacthefallenapple@gmail.com>
2023-11-21 14:08:39 +0100

    add next/previous tab keymaps

--- lua/timob/keymaps.lua
+++ lua/timob/keymaps.lua
@@ -30,3 +30,6 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+vim.keymap.set('n', '<leader><tab>', '<cmd>tabn<cr>', { desc = 'Go to next tab' })
+vim.keymap.set('n', '<leader><S-tab>', '<cmd>tabp<cr>', { desc = 'Go to previous tab' })
