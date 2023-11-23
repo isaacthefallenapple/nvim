85b4eba82f22bb2fec5b47c04427cc7e5b51525f
isaacthefallenapple <isaacthefallenapple@gmail.com>
2023-11-23 11:49:58 +0100

    add vim-vinegar style '-' keymap

--- lua/plugins/fileexplorer.lua
+++ lua/plugins/fileexplorer.lua
@@ -7,6 +7,12 @@ return {
         show_hidden = true,
       },
     },
+    config = function(_, opts)
+      local oil = require 'oil'
+      oil.setup(opts)
+
+      vim.keymap.set('n', '-', ':Oil<CR>', { desc = 'Open parent directory' })
+    end,
     -- Optional dependencies
     dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
