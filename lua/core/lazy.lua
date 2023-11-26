79b82bb9a7e42f06caccb512058e1fa371008198
isaacthefallenapple <isaacthefallenapple@gmail.com>
2023-11-26 16:03:49 +0100

    add hotpot (unfortunately)

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -14,6 +14,25 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Bootstap hotpot into lazy plugin dir if it does not exist yet.
+local hotpotpath = vim.fn.stdpath 'data' .. '/lazy/hotpot.nvim'
+if not vim.loop.fs_stat(hotpotpath) then
+  vim.notify('Bootstrapping hotpot.nvim...', vim.log.levels.INFO)
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    '--single-branch',
+    -- You may with to pin a known version tag with `--branch=vX.Y.Z`
+    '--branch=v0.9.6',
+    'https://github.com/rktjmp/hotpot.nvim.git',
+    hotpotpath,
+  }
+end
+
+-- As per lazy's install instructions, but insert hotpots path at the front
+vim.opt.runtimepath:prepend { hotpotpath, lazypath }
+
 -- [[ Configure plugins ]]
 -- NOTE: Here is where you install your plugins.
 --  You can configure plugins using the `config` key.
