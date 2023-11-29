413325542e6adf02d716ec2ff131028894e21ca6
isaacthefallenapple <isaacthefallenapple@gmail.com>
2023-11-29 12:46:42 +0100

    add efm and makeprg for swift files

--- after/ftplugin/swift.lua
+++ after/ftplugin/swift.lua
@@ -1,2 +1,6 @@
 vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
+
+vim.opt_local.errorformat = '%A%f:%l: %t%*[^:]: %m,%ZTest Case %.%# failed%.%#,%-GTest Case %.%#,%-GTest Suite %.%#,%C%m,%-G%.%#'
+
+vim.opt_local.makeprg = 'swift'
