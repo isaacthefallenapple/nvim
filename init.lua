8f1946fb3f3134d1c2f4f80558473393860cd2ff
isaacthefallenapple <isaacthefallenapple@gmail.com>
2024-06-05 16:51:27 +0200

    use kickstart's indent blankline

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ require 'core.options'
 require 'core.autocmds'
 require 'core.usercmds'
 require 'core.lazy'
+require 'kickstart.plugins.indent_line'
 
 -- See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
