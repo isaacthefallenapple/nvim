0c5c7611f1ea4e5864b158addaee687ad32cd18e
isaacthefallenapple <isaacthefallenapple@gmail.com>
2024-07-01 12:02:06 +0200

    remove indentline

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ require 'core.options'
 require 'core.autocmds'
 require 'core.usercmds'
 require 'core.lazy'
-require 'kickstart.plugins.indent_line'
 
 -- See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
