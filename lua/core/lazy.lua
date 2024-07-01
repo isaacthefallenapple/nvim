99697ebb44fa6b184831cc9544833e940fc5e965
isaacthefallenapple <isaacthefallenapple@gmail.com>
2024-07-01 12:37:43 +0200

    fix lazy

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,46 +1,27 @@
--- [[ Install `lazy.nvim` plugin manager ]]
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
 end
 vim.opt.rtp:prepend(lazypath)
-
--- Bootstap hotpot into lazy plugin dir if it does not exist yet.
-local hotpotpath = vim.fn.stdpath 'data' .. '/lazy/hotpot.nvim'
-if not vim.loop.fs_stat(hotpotpath) then
-  vim.notify('Bootstrapping hotpot.nvim...', vim.log.levels.INFO)
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    '--single-branch',
-    -- You may with to pin a known version tag with `--branch=vX.Y.Z`
-    '--branch=v0.9.6',
-    'https://github.com/rktjmp/hotpot.nvim.git',
-    hotpotpath,
-  }
-end
-
--- As per lazy's install instructions, but insert hotpots path at the front
-vim.opt.runtimepath:prepend { hotpotpath, lazypath }
-
--- [[ Configure plugins ]]
--- NOTE: Here is where you install your plugins.
---  You can configure plugins using the `config` key.
 --
---  You can also configure plugins after the setup call,
---    as they will be available in your neovim runtime.
-require('lazy').setup('plugins', {
-  change_detection = {
-    notify = false,
+-- Make sure to setup `mapleader` and `maplocalleader` before loading lazy.nvim
+-- so that mappings are correct.  This is also a good place to setup other
+-- settings (vim.opt)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+--
+-- Setup lazy.nvim
+require('lazy').setup {
+  spec = {
+    -- import your plugins
+    { import = 'plugins' },
   },
-})
+  -- Configure any other settings here. See the documentation for more
+  -- details.  colorscheme that will be used when installing plugins.
+  install = { colorscheme = { 'oxocarbon' } },
+  -- automatically check for plugin updates
+  checker = { enabled = false },
+  change_detection = { notify = false },
+}
